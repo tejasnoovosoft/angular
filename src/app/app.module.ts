a152edc7df7a2fcb3ebf5be7f15969f1b6350d72
tejasnoovosoft <tejas.noovosoft@gmail.com>
2023-12-13 11:12:52 +0530

    add home and cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {CartComponent} from './cart/cart.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {RouterModule, RouterOutlet} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: "home", component: HomeComponent, title: "Home | Ecomzy"},
+      {path: "cart", component: CartComponent, title: "Cart | Ecomzy"}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
