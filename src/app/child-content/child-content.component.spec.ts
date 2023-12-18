3e175792c90cb479f33212a3a47828bca27d186f
tejasnoovosoft <tejas.noovosoft@gmail.com>
2023-12-18 12:40:16 +0530

    add content-child component

---
+++ src/app/child-content/child-content.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChildContentComponent } from './child-content.component';
+
+describe('ChildContentComponent', () => {
+  let component: ChildContentComponent;
+  let fixture: ComponentFixture<ChildContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChildContentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ChildContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/child-content/child-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-content',
+  templateUrl: './child-content.component.html',
+  styleUrls: ['./child-content.component.css']
+})
+export class ChildContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
